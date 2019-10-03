5bc53d7d5ae4f0c252f35febf6540930e772f2cb
BryantH24 <gbhou5@gmail.com>
2019-10-03 12:05:53 -0500

    Added sentiment analysis, speech to text, text to speech, mp3 playing package

--- VRtest.py
+++ VRtest.py
@@ -3,16 +3,28 @@ from textblob import TextBlob
 from playsound import playsound
 from gtts import gTTS
 
-GOOGLE_CLOUD_SPEECH_CREDENTIALS = r"""
-"""
+import argparse
 
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
+
+GOOGLE_CLOUD_SPEECH_CREDENTIALS = 
 counter = 0
 
 def speaker(toTalk):
     tts = gTTS(toTalk)
     tts.save('speaking' + str(counter) + '.mp3')
     playsound('speaking' + str(counter) + '.mp3')
-speaker("good morning, tell me about your day")
+
+# speaker("Hello world.")
+# counter+=1
+# speaker("My name is hal9000")
+# counter+=1
+# speaker("I am here to listen")
+# counter+=1
+# speaker("Tell me about your feelings ")
+# counter+=1
 # obtain audio from the microphone
 
 def listener():
@@ -21,23 +33,38 @@ def listener():
         print("listening")
         audio = r.listen(source)
     try:
+        print("speech detected")
         speech = r.recognize_google_cloud(audio, credentials_json=GOOGLE_CLOUD_SPEECH_CREDENTIALS)
         return speech
     except sr.UnknownValueError:
         print("Google Cloud Speech could not understand audio")
     except sr.RequestError as e:
         print("Could not request results from Google Cloud Speech service; {0}".format(e))
-        print("You said: " + speech)
+
+
+
+
+
 speech = listener()
+print("You said: " + speech)
+
+content = speech
+client = language.LanguageServiceClient()
+document = types.Document(
+    content=content,
+    type=enums.Document.Type.PLAIN_TEXT)
+annotations = client.analyze_sentiment(document=document)
+magnitude = annotations.document_sentiment.magnitude
+print(magnitude)
 
 
-blob = TextBlob(speech)
-for sentence in blob.sentences:
-    print(sentence.sentiment.polarity)
-    happyness = sentence.sentiment.polarity
-if happyness > 0:
-    counter +=1
-    speaker("Seems like your day has been pretty good! Keep it up!")
-else:
-    counter +=2
-    speaker("Cheer up, its not too bad. There's always tomorrow!")
+# blob = TextBlob(speech)
+# for sentence in blob.sentences:
+#     print(sentence.sentiment.polarity)
+#     happyness = sentence.sentiment.polarity
+# if happyness > 0:
+#     counter +=1
+#     speaker("Seems like your day has been pretty good! Keep it up!")
+# else:
+#     counter +=2
+#     speaker("Cheer up, its not too bad. There's always tomorrow!")
