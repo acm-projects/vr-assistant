bb7f9d256496efdadcf9604d43d3886875f0d349
Cady Baltz <cadybaltz@gmail.com>
2019-10-06 02:30:31 -0500

    Added intents with DialogFlow and the ability for the conversation to continue

--- VRtest.py
+++ VRtest.py
@@ -9,7 +9,12 @@ from google.cloud import language
 from google.cloud.language import enums
 from google.cloud.language import types
 
-GOOGLE_CLOUD_SPEECH_CREDENTIALS = 
+import dialogflow
+from google.api_core.exceptions import InvalidArgument
+DIALOGFLOW_PROJECT_ID = 'virtualrealityassistant-uqfhte'
+DIALOGFLOW_LANGUAGE_CODE = 'en-US'
+GOOGLE_CLOUD_SPEECH_CREDENTIALS =
+
 counter = 0
 
 def speaker(toTalk):
@@ -42,20 +47,39 @@ def listener():
         print("Could not request results from Google Cloud Speech service; {0}".format(e))
 
 
+SESSION_ID = 'current-user-id'
+endConversation = ""
 
+#This checks to see if the conversation should continue
+while endConversation != "Goodbye":
 
+    speech = listener()
+    print("You said: " + speech)
 
-speech = listener()
-print("You said: " + speech)
+    content = speech
+    client = language.LanguageServiceClient()
+    document = types.Document(
+        content=content,
+        type=enums.Document.Type.PLAIN_TEXT)
+    annotations = client.analyze_sentiment(document=document)
+    magnitude = annotations.document_sentiment.magnitude
+    print(magnitude)
 
-content = speech
-client = language.LanguageServiceClient()
-document = types.Document(
-    content=content,
-    type=enums.Document.Type.PLAIN_TEXT)
-annotations = client.analyze_sentiment(document=document)
-magnitude = annotations.document_sentiment.magnitude
-print(magnitude)
+    #Code to determine intent
+    text_to_be_analyzed = speech
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
+    text_input = dialogflow.types.TextInput(text=text_to_be_analyzed, language_code=DIALOGFLOW_LANGUAGE_CODE)
+    query_input = dialogflow.types.QueryInput(text=text_input)
+    try:
+        response = session_client.detect_intent(session=session, query_input=query_input)
+    except InvalidArgument:
+        raise
+    print("Query text:", response.query_result.query_text)
+    print("Detected intent:", response.query_result.intent.display_name)
+    endConversation = str(response.query_result.intent.display_name)
+    print("Detected intent confidence:", response.query_result.intent_detection_confidence)
+    print("Fulfillment text:", response.query_result.fulfillment_text)
 
 
 # blob = TextBlob(speech)
