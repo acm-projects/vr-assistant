aba7fccad42ff104221fc97e52a3126792ffbcb3
Christina Abraham <40750751+christinaabraham@users.noreply.github.com>
2019-10-30 04:13:23 -0500

    Update ControllerScript.cs

--- Scripts/ControllerScript.cs
+++ Scripts/ControllerScript.cs
@@ -1,12 +1,12 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ControllerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
     public GameObject go;
-
+    
+    // Start is called before the first frame update
     void Start()
     {
         
