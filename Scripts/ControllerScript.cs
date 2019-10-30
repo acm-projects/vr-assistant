6ab52288cc9b3e19e4d481866f96f0c44444f9a3
Christina Abraham <40750751+christinaabraham@users.noreply.github.com>
2019-10-30 04:05:17 -0500

    added pointer scripts

---
+++ Scripts/ControllerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject go;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        transform.rotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTrackedRemote);
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            if(hit.collider != null) {
+                if( go != hit.collider.gameObject)
+                {
+                    go = hit.transform.gameObject;
+                    Debug.Log("On VR Raycast Enter");
+                }
+            }
+        }
+    }
+}
