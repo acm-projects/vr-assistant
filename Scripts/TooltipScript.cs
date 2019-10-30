394f708890824986e002699931bcd5e2782d15c8
Christina Abraham <40750751+christinaabraham@users.noreply.github.com>
2019-10-30 04:13:10 -0500

    Update TooltipScript.cs

--- Scripts/TooltipScript.cs
+++ Scripts/TooltipScript.cs
@@ -1,18 +1,26 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class tOOLTIPsCRIPT : MonoBehaviour
+public class TooltipScript : MonoBehaviour
 {
+    public Text helpText;
+    public Text tooltipText;
+
+    public string helpString;
+    public string tooltipString;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        helpText = GameObject.Find("HelpText").GetComponent<Text>();
+        tooltipText = GameObject.Find("tooltipText").GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
